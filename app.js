4476b3d082107a391da7ec9e12a7985511d901f7
crjake <crjake@gmail.com>
2021-07-17 16:30:03 +0100

    Added favicon.

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function main() {
 
     {
         const color = 0xFFFFFF;
-        const intensity = 1;
+        const intensity = 1.5;
         const light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
         scene.add(light);
